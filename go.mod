9183ae790b89a21188a02a591b398c346f5bf214
AFloresc <afloresc@edu.tecnocampus.cat>
2025-10-15 17:20:08 +0200

    Added markdown service and dependencies files

---
+++ go.mod
@@ -0,0 +1,9 @@
+module markdown-note-taking--app
+
+go 1.24.3
+
+require (
+	github.com/gomarkdown/markdown v0.0.0-20250810172220-2e2c11897d1a // indirect
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
+)
--- services/markdown.go
+++ services/markdown.go
@@ -1,1 +1,13 @@
 package services
+
+import (
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+)
+
+// RenderMarkdown conversion from Markdown to HTML
+func RenderMarkdown(md string) string {
+	opts := html.RendererOptions{Flags: html.CommonFlags}
+	renderer := html.NewRenderer(opts)
+	return string(markdown.ToHTML([]byte(md), nil, renderer))
+}
