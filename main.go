0654dc5ffcd98c62f790aa998c47173584d81da7
AFloresc <afloresc@edu.tecnocampus.cat>
2025-10-15 17:03:51 +0200

    Added endpoints: grammar-check POST, notees GET/POST andnotes/id/renderer

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	//Routes
+	r.HandleFunc("/grammar-check", handlers.CheckGrammar).Methods("POST")
+	r.HandleFunc("/notes", handlers.SaveNote).Methods("POST")
+	r.HandleFunc("/notes", handlers.ListNotes).Methods("GET")
+	r.HandleFunc("/notes/{id}/rendered", handlers.RenderNote).Methods("GET")
+
+	//Start server
+	log.Println("Servidor escuchando en http://localhost:8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
+}
